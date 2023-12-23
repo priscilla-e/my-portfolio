7d4a17daf0084a818aeb539db3f4574149c67efd
priscilla-e <56302477+priscilla-e@users.noreply.github.com>
2023-12-23 22:12:37 +0000

    Install Tailwind Css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-function App() {
+export default function App() {
 
   return (
     <>
-        <h1>My Portfolio </h1>
+        <h1 className='text-4xl text-red-900'>My Portfolio </h1>
     </>
   )
 }
-
-export default App
