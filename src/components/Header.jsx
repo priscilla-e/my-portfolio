ef0f9eeaa91d6d16d6061f3dd9bfc515cbb6e732
priscilla-e <56302477+priscilla-e@users.noreply.github.com>
2023-12-24 01:57:42 +0000

    Add react icons and update header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <header id="#header" className="flex items-center justify-center py-6 px-10 shadow-lg">
+        <header id="#header" className="hidden items-center py-6 px-10 shadow-lg md:flex">
             <div className="space-x-5 text-3xl tracking-tighter md:flex-1">
                 <span className="font-extrabold text-light">Priscilla</span>
                 <span>Emasoga</span>
@@ -8,7 +8,7 @@ export default function Header() {
 
             <nav>
                 {/* Desktop navigation menu */}
-                <ul className="hidden space-x-10 text-sm font-bold md:flex">
+                <ul className="flex space-x-10 text-sm font-bold">
                     <li className="active hover:text-light"><a href="#">About Me</a></li>
                     <li className="hover:text-light"><a href="#">Experience</a></li>
                     <li className="hover:text-light"><a href="#">Projects</a></li>
