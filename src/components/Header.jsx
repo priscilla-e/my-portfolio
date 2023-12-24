23b327edff0ab68033582452579fba8b4972ed2f
priscilla-e <56302477+priscilla-e@users.noreply.github.com>
2023-12-24 00:44:33 +0000

    Add header content and styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,20 @@
 export default function Header() {
     return (
-        <header>
-            <h1>My React App</h1>
+        <header id="#header" className="flex items-center justify-center py-6 px-10 shadow-lg">
+            <div className="space-x-5 text-3xl tracking-tighter md:flex-1">
+                <span className="font-extrabold text-light">Priscilla</span>
+                <span>Emasoga</span>
+            </div>
+
+            <nav>
+                {/* Desktop navigation menu */}
+                <ul className="hidden space-x-10 text-sm font-bold md:flex">
+                    <li className="hover:text-light"><a href="#">About Me</a></li>
+                    <li className="hover:text-light"><a href="#">Experience</a></li>
+                    <li className="hover:text-light"><a href="#">Projects</a></li>
+                    <li className="hover:text-light"><a href="#">My Blog</a></li>
+                </ul>
+            </nav>
         </header>
     )
 }
