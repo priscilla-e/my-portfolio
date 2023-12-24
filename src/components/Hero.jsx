7298602d57fc18ea80cd25281ff5ccbe20789fae
priscilla-e <56302477+priscilla-e@users.noreply.github.com>
2023-12-24 03:28:55 +0000

    Complete hero section styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ export default function Hero() {
         <section id="hero">
             <div className='container mx-auto my-10 flex flex-col items-center justify-center h-screen md:h-[90vh] md:flex-row-reverse md:justify-between md:my-0 md:space-x-24"'>
                 {/*image*/}
-                <img src={HeroImage} alt="Priscilla Emasoga" className="h-1/2 md:h-auto"/>
+                <img src={HeroImage} alt="Priscilla Emasoga" className="shadow-filter w-1/2 min-w-80 max-w-lg"/>
 
                 {/*hero text*/}
                 <div className='mt-10 space-y-6 text-center md:text-left md:m-0'>
@@ -16,7 +16,7 @@ export default function Hero() {
                         <span
                             className='inline-block bg-primary text-xs text-dark px-4 py-2 rounded font-semibold focus:outline-none'>UX/UI Designer</span>
                     </div>
-                    <h2 className='text-2xl font-bold'>Hi, I'm Priscilla</h2>
+                    <h2 className='text-2xl font-bold md:text-3xl xl:text-5xl'>Hi, I'm Priscilla</h2>
                     <p className='max-w-md'>
                         A Software Engineer and 2nd-year computer science student
                         at the University of Sheffield.
