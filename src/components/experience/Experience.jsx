958332a2b42d8f94a71ee09d8f0cdadf21507291
priscilla-e <56302477+priscilla-e@users.noreply.github.com>
2023-12-25 15:03:19 +0000

    Add experience desc design

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Button from '@mui/material/Button';
+
+import {EXPERIENCES} from '../../data.js'
 
 export default function Experience() {
-    const [activeTab, setActiveTab] = useState('nomura')
+    const [activeTab, setActiveTab] = useState(0)
 
     const tabStyle = (tab) => {
         if (tab === activeTab) {
-            return 'inline-flex items-center w-full h-12 p-4 uppercase border-r-2 text-primary border-r-primary active:text-primary active:bg-primary active:bg-opacity-5'
+            return 'inline-flex items-center  w-full h-12 p-4 uppercase border-r-2 text-sm text-primary border-r-primary active:text-primary active:bg-primary active:bg-opacity-5'
         } else {
-            return 'inline-flex items-center w-full h-12 p-4 uppercase border-r-2 border-r-darkLine active:text-primary active:bg-primary active:bg-opacity-5'
+            return 'inline-flex items-center w-full h-12 p-4 uppercase border-r-2  text-sm border-r-darkLine active:text-primary active:bg-primary active:bg-opacity-5'
         }
     }
 
@@ -20,16 +25,37 @@ export default function Experience() {
                     <span className='flex-1 hidden border-b border-b-darkLine md:inline-block'></span>
                 </div>
 
-                <div className="max-w-xs">
-                    <div className={tabStyle('nomura')} onClick={() => {setActiveTab('nomura')}}>
-                        Nomura
+                <div className="flex flex-col md:flex-row">
+
+                    {/*tabs*/}
+                    <div className="max-w-xs">
+                        {EXPERIENCES.map((experience, index) => (
+                            <button key={index} type='button' className={tabStyle(index)} onClick={() => {setActiveTab(index)}}>
+                                {experience.company}
+                            </button>
+                        ))}
                     </div>
-                    <div className={tabStyle('sheffield')} onClick={() => {setActiveTab('sheffield')}}>
-                        The University of Sheffield
+
+                    {/*tab content*/}
+                    <div className="flex-1 mt-6 md:mt-0 md:ml-10">
+                        <p className="font-bold space-x-2 md:text-2xl">
+                            <span className="">Technology Placement @</span>
+                            <span className="text-primary">Nomura</span>
+                        </p>
+                        <p className="mt-2 font-thin uppercase text-sm md:mt-4">
+                            <span>jul 2023</span>
+                            <span className="mx-2">-</span>
+                            <span>present</span>
+                        </p>
+                        <ul className='w-full mt-10 text-sm space-y-6 md:max-w-2xl'>
+                            {EXPERIENCES[activeTab].description.map((desc, index) => (
+                                <li key={index} className='styled-li'>
+                                    {desc}
+                                </li>
+                            ))}
+                        </ul>
                     </div>
-                    <div className={tabStyle('macroware')} onClick={() => {setActiveTab('macroware')}}>
-                        Macroware Inc.
-                    </div>
+
                 </div>
 
 
