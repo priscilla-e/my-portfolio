0a1b9ae22271454c218b67fc1bc018cbab607a11
priscilla-e <56302477+priscilla-e@users.noreply.github.com>
2023-12-25 15:02:52 +0000

    Add experiences data

--- src/data.js
+++ src/data.js
@@ -18,6 +18,33 @@ export const EXPERIENCES = [
     {
         company: 'Nomura',
         position: 'Technology Placement',
-        startDate: 'Jun 2023'
-    }
+        startDate: 'Jun 2023',
+        endDate: 'Present',
+        description: [
+            'Developed a Flask server to automate market load processes, leading to enhanced support for the global markets team',
+            'Designed and implemented a user-friendly web interface for the market server using React.js',
+            'Remediated a regression test viewer with over 50,000 failing test cases built on Excel using VBA',
+            'Developed robust error-handling procedures to effectively address all trivial test cases, significantly reducing the number of failing tests by 80%'
+        ]
+    },
+    {
+        company: 'Univeristy of Sheffield',
+        position: 'Software Developer',
+        startDate: 'Sep 2022',
+        endDate: 'Jun 2023',
+        description: [
+            "Worked as a part of a team to develop a Ruby on Rails application to streamline the management of student record change requests at the University of Sheffield",
+            "Implemented a user-friendly web interface using Bootstrap",
+            "Implemented various unit and end-to-end tests, enhancing the system's reliability and robustness",
+        ]
+    },
+    {
+        company: 'Engineers Without Borders UK',
+        position: 'Report Reviewer',
+        startDate: 'Jun 2022',
+        endDate: 'Jul 2022',
+        description: [
+            "Marked and gave feedback to innovative design concepts, submitted by other students for the Engineering for People Design Challenge 2022."
+        ]
+    },
 ]
