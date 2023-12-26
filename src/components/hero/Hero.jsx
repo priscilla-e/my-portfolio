4968dfbc59b790789e010b9fd498ed109ab74473
priscilla-e <56302477+priscilla-e@users.noreply.github.com>
2023-12-26 12:12:33 +0000

    Fix mobile styling issues

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -6,7 +6,7 @@ export default function Hero() {
     return (
         <section id="hero">
             <div
-                className='container mx-auto flex flex-col items-center h-screen my-10 md:h-[90vh] md:flex-row-reverse md:justify-between'>
+                className='container mx-auto flex flex-col items-center  mt-10 mb-24 md:h-[90vh] md:flex-row-reverse md:justify-between'>
                 {/*image*/}
                 <img src={HeroImage} alt="Priscilla Emasoga" className="shadow-filter w-1/2 min-w-60 md:min-w-80 max-w-lg"
                      data-aos="fade-left"/>
@@ -41,7 +41,7 @@ export default function Hero() {
                        // data-aos-duration="1600"
                     >
                         <MdEmail size='20' className='inline mr-2'/>
-                        Let's Chat
+                        Let&#39;s Chat
                     </a>
                 </div>
             </div>
