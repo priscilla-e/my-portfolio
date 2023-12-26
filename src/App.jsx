f32dc15732d36f4d494a68f46b1e494fed188973
priscilla-e <56302477+priscilla-e@users.noreply.github.com>
2023-12-26 11:31:11 +0000

    Utilize section observer to toggle active class in headers as page scrolls

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import {BrowserRouter} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import Header from './components/layout/Header.jsx'
 import Hero from "./components/hero/Hero.jsx";
 import About from "./components/about/About.jsx";
 import Experience from "./components/experience/Experience.jsx";
 import Projects from "./components/projects/Projects.jsx";
 import Footer from "./components/layout/Footer.jsx";
-
+import useSectionObserver from "./hooks/useSectionObserver.jsx";
+import {useEffect} from "react";
 export default function App() {
+    const navigate = useNavigate();
+    const activeSection = useSectionObserver(['hero', 'about', 'experience', 'projects']);
+
+    useEffect(() => {
+        if (activeSection) {
+            navigate(`#${activeSection}`)
+        }
+    }, [activeSection]);
 
     return (
-        <BrowserRouter>
-            <Header/>
+        <>
+            <Header activeSection={activeSection}/>
             <main>
                 <Hero/>
                 <About/>
@@ -18,6 +27,6 @@ export default function App() {
                 <Projects/>
             </main>
             <Footer/>
-        </BrowserRouter>
+        </>
     )
 }
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,6 +1,29 @@
-export default function Header() {
+import PropTypes from "prop-types";
+export default function Header({activeSection}) {
+
+    console.log(activeSection);
+
+    const menuItems = [
+        {name: 'Home', 'link': '#'},
+        {name: 'About Me', 'link': '#about'},
+        {name: 'Experience', 'link': '#experience'},
+        {name: 'Projects', 'link': '#projects'},
+    ]
+
+    const isActive = (link, name) => {
+        const section = '#'+activeSection;
+        if (link === section) {
+            return 'active'
+        }
+        else if (name === 'Home' && section === '#hero') {
+            return 'active'
+        }
+    }
+
     return (
-        <header id="#header" className="hidden sticky top-0 !z-10 items-center py-4 px-10 shadow-lg md:flex" data-aos="fade-down">
+        <header id="#header"
+                className="hidden sticky top-0 !z-10 items-center py-4 px-10 shadow-lg bg-dark bg-opacity-90 md:flex"
+                data-aos="fade-down">
             <div className="space-x-5 text-3xl tracking-tighter md:flex-1">
                 <span className="font-extrabold text-light">Priscilla Emasoga</span>
                 {/*<span>Emasoga</span>*/}
@@ -9,13 +32,20 @@ export default function Header() {
             <nav>
                 {/* Desktop navigation menu */}
                 <ul className="flex space-x-10 text-sm font-bold">
-                    <li className="active hover:text-light"><a href='#'>Home</a></li>
-                    <li className="hover:text-light"><a href="#about">About Me</a></li>
-                    <li className="hover:text-light"><a href="#experience">Experience</a></li>
-                    <li className="hover:text-light"><a href="#projects">Projects</a></li>
-                    <li className="hover:text-light"><a href="#">My Blog</a></li>
+                    {menuItems.map((item, index) => (
+                        <li key={index}
+                            className={`hover:text-light ${isActive(item.link, item.name)}`}>
+                            <a href={`${item.link}`}>
+                                {item.name}
+                            </a>
+                        </li>
+                    ))}
                 </ul>
             </nav>
         </header>
     )
+}
+
+Header.propTypes = {
+    activeSection: PropTypes.string.isRequired,
 }
