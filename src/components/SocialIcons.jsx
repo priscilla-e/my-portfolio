a23f7dfc9e93078805300d1c58c13923fdd63575
priscilla-e <56302477+priscilla-e@users.noreply.github.com>
2023-12-27 00:28:12 +0000

    Update icons classes

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -6,16 +6,28 @@ export default function SocialIcons(){
     return(
         <div className="fixed right-0 bottom-0 left-0 bg-darkLine !z-40 md:left-auto md:bottom-auto md:top-1/2 md:-translate-y-1/2">
             <div className="flex items-center justify-center p-4 space-x-4 text-3xl md:inline-flex md:flex-col md:space-x-0 md:space-y-4">
-                <a href="mailto:priscillaemasoga[at]gmail[dot]com">
+                <a href="mailto:priscillaemasoga[at]gmail[dot]com" className='hover:text-primary duration-300'>
                     <MdEmail/>
                 </a>
-                <a href='https://github.com/priscilla-e' target='_blank' rel='noreferrer'>
+                <a href='https://github.com/priscilla-e'
+                   target='_blank'
+                   rel='noreferrer'
+                   className='hover:text-primary duration-300'
+                >
                     <FaGithub/>
                 </a>
-                <a href='https://www.linkedin.com/in/priscillaemasoga/' target='_blank' rel='noreferrer'>
+                <a href='https://www.linkedin.com/in/priscillaemasoga/'
+                   target='_blank'
+                   rel='noreferrer'
+                   className='hover:text-primary duration-300'
+                >
                     <FaLinkedin/>
                 </a>
-                <a href='https://www.instagram.com/cilla.dev/' target='_blank' rel='noreferrer'>
+                <a href='https://www.instagram.com/cilla.dev/'
+                   target='_blank'
+                   rel='noreferrer'
+                   className='hover:text-primary duration-300'
+                >
                     <RiInstagramFill/>
                 </a>
 
